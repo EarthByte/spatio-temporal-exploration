2880ac8328bc91da0967c77b42b344faa2ad11a7
michaelchin <michael.chin@sydney.edu.au>
2020-03-24 17:16:48 +1100

    update 2_Plot_and_Select_Mineral_Resources.ipynb

--- python/create_coregistration_input_data_example.py
+++ python/create_coregistration_input_data_example.py
@@ -28,29 +28,15 @@ def get_time_from_age(ages, start, end, step):
             if not (mod < step/2.):
                 idx = idx+1 
             ret.append(times[idx])
-    return ret 
+    return ret
 
-def run_it():
-    start_time = param["time"]["start"]
-    end_time = param["time"]["end"]
-    time_step =  param["time"]["step"]
-
-    if not os.path.isfile('./convergence_data/subStats_0.00.csv'):
-        sys.exit('ERROR!!! File ./convergence_data/subStats_0.00.csv not found! Run convergence.py first!')
-
+#
+def process_real_deposits(start_time, end_time, time_step):
     if not os.path.isfile('../data/CopperDeposits/XYBer14_t2_ANDES.shp'):
         sys.exit('ERROR!!! File ../data/CopperDeposits/XYBer14_t2_ANDES.shp not found! Find the file or change the code to use another file!')
-
-
-    #load files
-    f = np.loadtxt('./convergence_data/subStats_0.00.csv') #all subduction points at time 0
-    #TODO: choose the points in which you are interested
-    trench_points=f[(f[:,9])==201] #subduction points in south america 
-
     reader = shapefile.Reader('../data/CopperDeposits/XYBer14_t2_ANDES.shp')
     recs    = reader.records()
     andes_points_len = len(recs)
-    randomAges=np.random.randint(start_time+1, end_time, size=andes_points_len) #generate random ages for andes data
     times = get_time_from_age(np.array(recs)[:,6], start_time, end_time, time_step)#get integer ages for andes data
 
     #index, lon, lat, time, plate id
@@ -58,34 +44,65 @@ def run_it():
 
     # andes deposit points with the real age
     for i in range(andes_points_len):
-        data.append([i, recs[i][3], recs[i][4], times[i], recs[i][7]])
-
-    points_with_age_size=i+1
-
-    # andes deposit points with random ages
-    for i in range(andes_points_len): 
-        data.append([points_with_age_size+i, recs[i][3], recs[i][4], randomAges[i], recs[i][7] ])
-
-    points_with_random_age_size=i+1
-
-    # trench points for each time step from start_time to end_time
-    start_idx = points_with_age_size + points_with_random_age_size
+        data.append([i, recs[i][3], recs[i][4], times[i], recs[i][7]]) 
+    return data
+ 
+#
+def generate_random_deposits(data, start_time, end_time):
+    random_data=[]
+    randomAges=np.random.randint(start_time+1, end_time, size=len(data)) #generate random ages for andes data
+     # andes deposit points with random ages
+    for i in range(len(data)): 
+        random_data.append([i, data[i][1], data[i][2], randomAges[i], data[i][4] ])
+    return random_data
+ 
+#                                 
+def generate_trench_points(start_time, end_time, time_step):
+    trench_data=[]
+    if not os.path.isfile('./convergence_data/subStats_0.00.csv'):
+        sys.exit('ERROR!!! File ./convergence_data/subStats_0.00.csv not found! Run convergence.py first!')
+    #load files
+    f = np.loadtxt('./convergence_data/subStats_0.00.csv') #all subduction points at time 0
+    #TODO: choose the points in which you are interested
+    trench_points=f[(f[:,9])==201] #subduction points in south america 
     i=0
-    for p in trench_points:
-        for t in range(end_time):
-            data.append([start_idx+i, p[0], p[1], t, 201]) 
-            i+=1
+    for t in range(start_time, end_time, time_step):
+        for p in trench_points:
+            trench_data.append([i, p[0], p[1], t, 201]) # we de
+            i+=1   
+    return trench_data    
 
+#
+def save_data(data,fn):
     # data are ready and write them to file
-    with open('coregistration_input_data_example.csv',"w+") as f:
+    with open(fn,"w+") as f:
+        f.write('index,lon,lat,age,plate_id\n')
         for row in data:
             #print(row)
             if row:
-                f.write('{0:d}, {1:.2f}, {2:.2f}, {3:d}, {4:d}'.format(row[0],row[1],row[2],row[3],row[4]))
+                f.write('{0:d}, {1:.2f}, {2:.2f}, {3:d}, {4:d}'.format(
+                    int(row[0]),float(row[1]),float(row[2]),int(row[3]),int(row[4])))
             f.write('\n')
 
-    print('The data have been written into coregistration_input_data_example.csv successfully!')
+    print(f'The data have been written into {fn} successfully!')                               
 
 if __name__ == '__main__':
-    run_it() 
-    
+    start_time = param["time"]["start"]
+    end_time = param["time"]["end"]
+    time_step =  param["time"]["step"]
+                                 
+    data = process_real_deposits(start_time, end_time, time_step)
+                                 
+    random_data = generate_random_deposits(data, start_time, end_time)
+                                 
+    trench_data = generate_trench_points(start_time, end_time, time_step)
+                                 
+    all_data = data+random_data+trench_data
+    for i in range(len(all_data)): 
+        all_data[i][0] = i #assign correct indices
+                                 
+    save_data(all_data, 'coregistration_input_data_example.csv')
+                                 
+                                 
+                                 
+                                 
